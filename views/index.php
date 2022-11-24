87f253d5ce76a4196399d0e3d285ff239baf8c9e
Diana <dianadauksa@gmail.com>
2022-11-25 00:29:20 +0200

    Update index.php

--- views/index.php
+++ views/index.php
@@ -11,7 +11,7 @@ use function App\{
 <!doctype html>
 <html lang="en">
 <head>
-    <link rel="stylesheet" href="styles/styles.css"/>
+    <link rel="stylesheet" href="../styles/styles.css"/>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700;800&display=swap"
